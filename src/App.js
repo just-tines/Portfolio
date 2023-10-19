5058d197cdc940c116f058a3e72d861237e737b4
justine <markjustinealvarez1@gmail.com>
2023-10-19 14:19:32 -0700

    another commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-// import Hero from './components/Hero';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import Project from './components/Project';
-import Newsletter from './components/Newsletter';
-import Experience from './components/Experience';
-
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Project from "./components/Project";
+import Newsletter from "./components/Newsletter";
+import Experience from "./components/Experience";
 
 export default function App() {
-return (
-  <div>
-    <NavBar />
-    <Home />
-    <Experience />
-    <Project />
-    <Newsletter />
-    
-  </div>
-    
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <Experience />
+      <Project />
+      <Newsletter />
+    </div>
   );
 }
